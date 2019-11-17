536cb1e9e30e2cf929534254258e96ab02cf0539
AustinPotts <pottsevolvements@gmail.com>
2019-11-17 10:08:21 -0500

    Configured Custom Layout
    Starter design for project.

---
+++ SwiftNewsKit-Design1/CustomData.swift
@@ -0,0 +1,16 @@
+//
+//  CustomData.swift
+//  SwiftNewsKit-Design1
+//
+//  Created by Austin Potts on 11/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CustomData{
+    var title: String
+    var image: UIImage
+    var url: String
+}
